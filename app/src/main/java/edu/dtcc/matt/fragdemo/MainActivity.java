34b4f18eb96be1187d4c7ac3d0ec6df49a4c7bbc
Matt <mdeascanis@hotmail.com>
2017-02-28 23:11:17 -0500

    User interface working

--- app/src/main/java/edu/dtcc/matt/fragdemo/MainActivity.java
+++ app/src/main/java/edu/dtcc/matt/fragdemo/MainActivity.java
@@ -1,13 +1,19 @@
 package edu.dtcc.matt.fragdemo;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements InputFrag.OnFragmentInteractionListener, OutputFrag.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
